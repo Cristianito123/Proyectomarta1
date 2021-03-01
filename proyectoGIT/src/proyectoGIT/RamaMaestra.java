019fa67d54fe7328ac3a719fdc53f77a6692be36
Cristian <Cristian@DESKTOP-T31V0B4>
2021-03-01 09:16:29 +0100

    MODIFICACION RAMA 1

--- proyectoGIT/src/proyectoGIT/RamaMaestra.java
+++ proyectoGIT/src/proyectoGIT/RamaMaestra.java
@@ -3,5 +3,6 @@ package proyectoGIT;
 public class RamaMaestra {
 	public static void main(String[] args) {
 		System.out.println("Esta es la rama maestra");
+		System.out.println("SOY LA RAMA 1");
 	}
 }
